990e5b0c5d3673dd5c076f8b1f44e55a97e7ace0
Mateus Sales <uMateusSales@github.com>
2023-10-11 01:04:31 -0300

    feat: fluig toast, função de add e remover o produto

--- scripts/classesDoFluig.js
+++ scripts/classesDoFluig.js
@@ -1,3 +1,5 @@
+/* Adicionar alguns estilos do fluig */
+
 const addColorPreta = () => {
   const labels = document.querySelectorAll("label");
 
@@ -7,3 +9,49 @@ const addColorPreta = () => {
 };
 
 addColorPreta();
+
+
+
+
+
+const addFluigToast = (title, type, timeout) =>{
+  FLUIGC.toast({
+    title: `${title}`,
+   message: '',
+    type: `${type}`,
+    timeout: `${timeout}`,
+    });
+  
+
+}
+
+
+
+
+
+
+
+
+
+/* Botão de adicionar produto */
+$("#addProduto").click(function () {
+  addFluigToast('Produto adicionado', 'success' , 'fast')
+  $("#produtoCard").clone(true).appendTo("#produtoContainer");
+});
+
+
+
+
+/* Deletar produto */
+$(".deletarProduto").on("click", function () {
+  const produtos = $(".produtoClass");
+
+  if (produtos.length > 1) {
+     $(this).closest("#produtoCard").remove();
+
+ 
+
+  } else  addFluigToast('Precisa haver no minimo 1 produto', 'warning' , 'fast')
+  
+});
+
