07ed5af4c14470e9e80cf67388170b4ffb96c134
Mateus Sales <mateus.lins.sales@gmail.com>
2023-10-11 12:00:11 -0300

    feat: layout terminado

--- scripts/classesDoFluig.js
+++ scripts/classesDoFluig.js
@@ -10,48 +10,43 @@ const addColorPreta = () => {
 
 addColorPreta();
 
-
-
-
-
-const addFluigToast = (title, type, timeout) =>{
+const addFluigToast = (title, type, timeout) => {
   FLUIGC.toast({
     title: `${title}`,
-   message: '',
+    message: "",
     type: `${type}`,
     timeout: `${timeout}`,
-    });
-  
-
-}
-
-
-
-
-
-
-
-
+  });
+};
 
 /* Botão de adicionar produto */
 $("#addProduto").click(function () {
-  addFluigToast('Produto adicionado', 'success' , 'fast')
+  const produtos = $(".produtoClass");
+
+  addFluigToast("Produto adicionado", "success", "fast");
   $("#produtoCard").clone(true).appendTo("#produtoContainer");
 });
-
-
-
 
 /* Deletar produto */
 $(".deletarProduto").on("click", function () {
   const produtos = $(".produtoClass");
 
   if (produtos.length > 1) {
-     $(this).closest("#produtoCard").remove();
-
- 
-
-  } else  addFluigToast('Precisa haver no minimo 1 produto', 'warning' , 'fast')
-  
+    $(this).closest("#produtoCard").remove();
+  } else addFluigToast("Precisa haver no minimo 1 produto", "warning", "fast");
 });
 
+$("#addAnexo").click(function () {
+  addFluigToast("Documento adicionado", "success", "fast");
+  $("#documentoCard").clone(true).appendTo("#anexoContainer");
+});
+
+$(".btnDelDocumento").on("click", function () {
+  const documentos = $(".documentoClass");
+
+  if (documentos.length > 1) {
+    $(this).closest(".documentoClass").remove();
+  } else addFluigToast("Precisa haver no minimo 1 documento", "warning", "fast");
+});
+const form = document.forms[0];
+console.log(form);
